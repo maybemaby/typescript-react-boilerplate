8f4f04378d63f14fc3f0c9d174696e00d7fcaa4b
maybemaby <74226064+maybemaby@users.noreply.github.com>
2022-06-12 17:07:18 -0700

    Add eslint-plugin-recat

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
